b2483f0725042e1b33ee507a3dacfbc1bc04a826
FazilAbdhulla <200528768@student.georgianc.on.ca>
2024-04-04 21:59:31 -0400

    committing the submission files

---
+++ wp-content/plugins/woo-usps-simple-shipping/vendor/composer/capsuled.php
@@ -0,0 +1,38 @@
+<?php return array (
+  'capsule' => 
+  array (
+    'class' => 
+    array (
+      'Dgm\\WcTools\\WcTools' => 'Dgm\\UspsSimple\\Vendors\\Dgm\\WcTools\\WcTools',
+      'DgmWpPluginBootstrapGuard' => 'Dgm_UspsSimple_Vendors_DgmWpPluginBootstrapGuard',
+      'DgmWpDismissibleNotices' => 'Dgm_UspsSimple_Vendors_DgmWpDismissibleNotices',
+      'Composer\\Autoload\\ComposerStaticInit0bc252d3b5d7545c5cec5dcedb62f4a1' => 'Dgm\\UspsSimple\\Vendors\\Composer\\Autoload\\ComposerStaticInit0bc252d3b5d7545c5cec5dcedb62f4a1',
+      'Composer\\Autoload\\ClassLoader' => 'Dgm\\UspsSimple\\Vendors\\Composer\\Autoload\\ClassLoader',
+      'ComposerAutoloaderInit0bc252d3b5d7545c5cec5dcedb62f4a1' => 'Dgm_UspsSimple_Vendors_ComposerAutoloaderInit0bc252d3b5d7545c5cec5dcedb62f4a1',
+    ),
+    'func' => 
+    array (
+    ),
+    'const' => 
+    array (
+    ),
+  ),
+  'uncapsule' => 
+  array (
+    'class' => 
+    array (
+      'Dgm\\UspsSimple\\Vendors\\Dgm\\WcTools\\WcTools' => 'Dgm\\WcTools\\WcTools',
+      'Dgm_UspsSimple_Vendors_DgmWpPluginBootstrapGuard' => 'DgmWpPluginBootstrapGuard',
+      'Dgm_UspsSimple_Vendors_DgmWpDismissibleNotices' => 'DgmWpDismissibleNotices',
+      'Dgm\\UspsSimple\\Vendors\\Composer\\Autoload\\ComposerStaticInit0bc252d3b5d7545c5cec5dcedb62f4a1' => 'Composer\\Autoload\\ComposerStaticInit0bc252d3b5d7545c5cec5dcedb62f4a1',
+      'Dgm\\UspsSimple\\Vendors\\Composer\\Autoload\\ClassLoader' => 'Composer\\Autoload\\ClassLoader',
+      'Dgm_UspsSimple_Vendors_ComposerAutoloaderInit0bc252d3b5d7545c5cec5dcedb62f4a1' => 'ComposerAutoloaderInit0bc252d3b5d7545c5cec5dcedb62f4a1',
+    ),
+    'func' => 
+    array (
+    ),
+    'const' => 
+    array (
+    ),
+  ),
+);
